a082ebcb4ddea52223444048c9f07c83eb31ffb8
LeBeaufort <135138712+LeBeaufort@users.noreply.github.com>
2024-06-18 19:41:30 +0200

    is not working

--- game.py
+++ game.py
@@ -2,10 +2,14 @@ import curses
 from bigtext import *
 import datetime
 from time import sleep
+from curses.textpad import rectangle
 
 
 def main(stdscr):
     in_menu = True
+
+    GAME_SIZE_X = 28
+    GAME_SIZE_Y = 25
 
     stdscr.nodelay(True)  # make stdscr.getkey() non-blocking
     curses.noecho()
@@ -24,8 +28,10 @@ def main(stdscr):
         stdscr.addstr(0, 0, 'Your terminal seems too little to play this game, you should get a bigger one !',
                       curses.color_pair(1))
 
-    while True:
+    #  so we are sure to do not display anything outside an area
+    game_window = curses.newwin(GAME_SIZE_Y, GAME_SIZE_Y, 3, 3)
 
+    while True:
 
         #  get key input
         try:
@@ -37,7 +43,7 @@ def main(stdscr):
         if key == " ":
             in_menu = False
             # stuff to run the game
-        elif key == '\x1b': # if the user press escape, we leave the game
+        elif key == '\x1b':  # if the user press escape, we leave the game
             curses.endwin()
             exit()
 
@@ -46,12 +52,14 @@ def main(stdscr):
         if in_menu:
             # display the intro
             stdscr.addstr(1, center_y - 39, intro, curses.color_pair(2))
-
             stdscr.addstr(10, 39, play, curses.color_pair(3))
             stdscr.addstr(20, 39, exitmsg, curses.color_pair(3))
         else:
-            pass
-            # todo : game here
+            try:
+                rectangle(game_window, 1, 1, 4, 4)
+            except:
+                pass  # ignoring the error :
+                # https://stackoverflow.com/questions/52804155/extending-curses-rectangle-box-to-edge-of-terminal-in-python
 
         # displaying the where source code is
         stdscr.addstr(0, 0, "Source code available on https://github.com/LeBeaufort/ssh-game", curses.color_pair(4))
