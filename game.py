6572243636cf242281bdf593056174a980b10f22
LeBeaufort <135138712+LeBeaufort@users.noreply.github.com>
2024-06-24 17:41:08 +0200

    game over + snake moving

--- game.py
+++ game.py
@@ -3,12 +3,14 @@ from bigtext import *
 from time import sleep
 from curses.textpad import rectangle
 
+default_snake = [(9, 3), (9, 4), (9, 5)]
 snake = [(9, 3), (9, 4), (9, 5)]
 direction = 0  # 0 --> UP ; 1 --> LEFT ; 2 --> DOWN ; 3 --> RIGHT
 
 
 def update_direction(new_key):
     global direction
+    print(f"Direction is {direction}")
     if new_key == "UP" and direction != 2:
         direction = 0
     elif new_key == "LEFT" and direction != 3:
@@ -17,6 +19,7 @@ def update_direction(new_key):
         direction = 2
     elif new_key == "RIGHT" and direction != 1:
         direction = 3
+    print(f"Now it is [{direction}")
 
 
 def display_snake(window):
@@ -27,7 +30,26 @@ def display_snake(window):
                 window.addstr(y * 4 + a, x * 4 + b, "#", curses.color_pair(5))
 
 
+def update_snake():
+    global direction
+    global snake
+    headx = snake[0][0]
+    heady = snake[0][1]
+    if direction == 0:
+        heady -= 1
+    elif direction == 1:
+        headx += 1
+    elif direction == 2:
+        heady += 1
+    else:
+        headx -= 1
+
+    snake.insert(0, (headx, heady))
+    snake.pop(len(snake)-1)
+
+
 def main(stdscr):
+    global snake, default_snake
     in_menu = True
 
     GAME_SIZE_X = 68
@@ -62,11 +84,13 @@ def main(stdscr):
         # do something with it
         if key == " ":
             in_menu = False
+            snake = default_snake.copy()
         elif key == '\x1b':  # if the user press escape, we leave the game
             curses.endwin()
             exit()
         # stuff to update the direction of the snake
         elif key == curses.KEY_UP:
+            print("HELLOOOOOOOOO")
             update_direction("UP")
         elif key == curses.KEY_DOWN:
             update_direction("DOWN")
@@ -84,9 +108,15 @@ def main(stdscr):
             stdscr.addstr(10, 0, play, curses.color_pair(3))
             stdscr.addstr(20, 0, exitmsg, curses.color_pair(3))
         else:
-            try:
-                game_window.clear()
+            update_snake()
+            game_window.clear()
+            if snake[0][0] > GAME_SIZE_X/4 or snake[0][1] > GAME_SIZE_Y/4 or snake[0][0] <= 0 or snake[0][1] <= 0:
+                game_window.addstr(int(GAME_SIZE_Y/2-5), 5, gameover, curses.color_pair(1))
+                in_menu = True
+            else:
                 display_snake(game_window)
+
+            try:
                 rectangle(game_window, 0, 0, GAME_SIZE_Y - 1, GAME_SIZE_X - 1)
             except:
                 pass
@@ -99,7 +129,7 @@ def main(stdscr):
         stdscr.refresh()
         game_window.refresh()  # we need it
         stdscr.getch()
-        sleep(0.1)
+        sleep(0.5)
 
 
 curses.wrapper(main)
